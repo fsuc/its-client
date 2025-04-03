c90cbecaa41cb947fcae9c970c823fb2ccddde7e
François Suc <francois.suc@orange.com>
2025-04-03 10:09:00 +0200

    swift: add region of interest
    Signed-off-by: François Suc <francois.suc@orange.com>

---
+++ swift/ITSClient/Sources/ITSMobility/RegionOfInterest/RegionOfInterestCoordinator.swift
@@ -0,0 +1,91 @@
+/*
+ * Software Name : ITSClient
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * see the "LICENSE.txt" file for more details or https://opensource.org/license/MIT/
+ *
+ * Software description: Swift ITS client.
+ */
+
+import Foundation
+
+final class RegionOfInterestCoordinator {
+    struct TopicUpdateRequest {
+        let subscriptions: [String]
+        let unsubscriptions: [String]
+    }
+
+    private let quadkeyBuilder = QuadkeyBuilder()
+    private var currentDENMRegionOfInterest: RegionOfInterest?
+
+    func updateAlarmRegionOfInterest(
+        latitude: Double,
+        longitude: Double,
+        zoomLevel: Int
+    ) -> TopicUpdateRequest? {
+        updateRegionOfInterest(for: .denm,
+                               latitude: latitude,
+                               longitude: longitude,
+                               zoomLevel: zoomLevel,
+                               currentRegionOfInterest: &currentDENMRegionOfInterest)
+    }
+
+
+    private func updateRegionOfInterest(
+        for messageType: MessageType,
+        latitude: Double,
+        longitude: Double,
+        zoomLevel: Int,
+        currentRegionOfInterest: inout RegionOfInterest?) -> TopicUpdateRequest? {
+        let quadkey = quadkeyBuilder.quadkeyFrom(latitude: latitude,
+                                                 longitude: longitude,
+                                                 zoomLevel: zoomLevel,
+                                                 separator: "/")
+
+        guard quadkey != currentRegionOfInterest?.quadkey else {
+            return nil
+        }
+
+        let neighborQuadkeys = quadkeyBuilder.neighborQuadkeys(for: quadkey)
+        let regionOfInterest = RegionOfInterest(quadkey: quadkey,
+                                                neighborQuadkeys: neighborQuadkeys)
+        let topicUpdate = updateTopicSubscriptions(newRegionOfInterest: regionOfInterest,
+                                                   currentRegionOfInterest: currentRegionOfInterest,
+                                                   for: messageType)
+        currentRegionOfInterest = regionOfInterest
+
+        return topicUpdate
+    }
+
+    private func updateTopicSubscriptions(
+        newRegionOfInterest: RegionOfInterest,
+        currentRegionOfInterest: RegionOfInterest?,
+        for messageType: MessageType
+    ) -> TopicUpdateRequest {
+        var subscriptions = [String]()
+        var unsubscriptions = [String]()
+
+        let newQuadkeys = newRegionOfInterest.allQuadkeys
+        let currentQuadkeys = currentRegionOfInterest?.allQuadkeys ?? []
+
+        newQuadkeys.forEach { newQuadkey in
+            if !currentQuadkeys.contains(newQuadkey) {
+                subscriptions.append(topic(for: messageType, in: newQuadkey))
+            }
+        }
+
+        currentQuadkeys.forEach { currentQuadkey in
+            if !newQuadkeys.contains(currentQuadkey) {
+                unsubscriptions.append(topic(for: messageType, in: currentQuadkey))
+            }
+        }
+
+        return TopicUpdateRequest(subscriptions: subscriptions, unsubscriptions: unsubscriptions)
+    }
+
+    private func topic(for messageType: MessageType, in quadkey: String) -> String {
+        "/outQueue/v2x/\(messageType)/+/\(quadkey)"
+    }
+}
---
+++ swift/ITSClient/Tests/ITSMobilityTests/RegionOfInterestCoordinatorTests.swift
@@ -0,0 +1,74 @@
+/*
+ * Software Name : ITSClient
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * see the "LICENSE.txt" file for more details or https://opensource.org/license/MIT/
+ *
+ * Software description: Swift ITS client.
+ */
+
+import Foundation
+import Testing
+@testable import ITSMobility
+
+struct RegionOfInterestCoordinatorTests {
+    @Test("RegionOfInterestCoordinator should return 9 subscriptions and 0 unsubscription the first time")
+    func regionofinterestcoordinator_should_return_9_subscriptions_and_0_unsubscription_first_time() throws {
+        // Given
+        let regionOfInterestCoordinator = RegionOfInterestCoordinator()
+
+        // When
+        let requestUpdate = regionOfInterestCoordinator.updateAlarmRegionOfInterest(
+            latitude: 43.63516355648167,
+            longitude: 1.3744570239910097,
+            zoomLevel: 22)
+
+        // Then
+        let unwrapRequestUpdate = try #require(requestUpdate)
+        #expect(unwrapRequestUpdate.subscriptions.count == 9)
+        #expect(unwrapRequestUpdate.unsubscriptions.isEmpty)
+    }
+
+    @Test("RegionOfInterestCoordinator should return 9 subscriptions and 9 unsubscriptions when updating")
+    func regionofinterestcoordinator_should_return_9_subscriptions_and_9_unsubscriptions_when_updating() throws {
+        // Given
+        let regionOfInterestCoordinator = RegionOfInterestCoordinator()
+        _ = regionOfInterestCoordinator.updateAlarmRegionOfInterest(
+            latitude: 43.63516355648167,
+            longitude: 1.3744570239910097,
+            zoomLevel: 22)
+
+        // When
+        let requestUpdate = regionOfInterestCoordinator.updateAlarmRegionOfInterest(
+            latitude: 43.64516355648167,
+            longitude: 1.3844570239910097,
+            zoomLevel: 22)
+
+        // Then
+        let unwrapRequestUpdate = try #require(requestUpdate)
+        #expect(unwrapRequestUpdate.subscriptions.count == 9)
+        #expect(unwrapRequestUpdate.unsubscriptions.count == 9)
+    }
+
+    @Test("RegionOfInterestCoordinator should return nil when no update")
+    func regionofinterestcoordinator_should_return_nil_when_no_update() throws {
+        // Given
+        let regionOfInterestCoordinator = RegionOfInterestCoordinator()
+        _ = regionOfInterestCoordinator.updateAlarmRegionOfInterest(
+            latitude: 43.63516355648167,
+            longitude: 1.3744570239910097,
+            zoomLevel: 22)
+
+        // When
+        let requestUpdate = regionOfInterestCoordinator.updateAlarmRegionOfInterest(
+            latitude: 43.63516355648101,
+            longitude: 1.3744570239910001,
+            zoomLevel: 22)
+
+        // Then
+        #expect(requestUpdate == nil)
+    }
+}
+
